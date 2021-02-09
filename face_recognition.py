a2c908c74d872b79821120dd4cac384fc8509ca9
Mitchell Kiscadden <38437417+kiscadma@users.noreply.github.com>
2021-02-08 22:13:51 -0500

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -7,14 +7,13 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '4'
 
 def learn(target_name="George_W_Bush", target_count=530):
     all_data = []
-    input_shape = (100, 100, 1)
-    target_size = (100, 100) 
-    color_mode = "grayscale"
+    input_shape = (120, 120, 1)
+    target_size = (120, 120) 
 
     # grab all pictures of the target and add them with label 1
     pics = glob.glob('lfw/' + target_name + '/*.jpg')
     for pic in pics:
-        all_data.append((img_to_array(load_img(pic, color_mode=color_mode, target_size=target_size)), 1))
+        all_data.append((img_to_array(load_img(pic, color_mode="grayscale", target_size=target_size)), 1))
 
     f = open('lfw-names.txt')
     for line in f:
@@ -26,7 +25,7 @@ def learn(target_name="George_W_Bush", target_count=530):
         # add all photos for the non-target person with the 0 label
         pics = glob.glob('lfw/' + info[0] + '/*.jpg')
         for pic in pics:
-            all_data.append((img_to_array(load_img(pic, color_mode=color_mode, target_size=target_size)), 0))
+            all_data.append((img_to_array(load_img(pic, color_mode="grayscale", target_size=target_size)), 0))
 
         # make sure we have an even split between target and non-target
         # having too many of either will skew training data
@@ -45,8 +44,10 @@ def learn(target_name="George_W_Bush", target_count=530):
     # add some of data to the training data sets
     for pic_arr, lbl in all_data[:train_test_split]:
         trn_data.append(pic_arr)
+        trn_lbls.append(lbl)
         trn_data.append(random_rotation(pic_arr, 15))  # add a rotated version of the picture
         trn_lbls.append(lbl)
+        trn_data.append(random_rotation(pic_arr, 15))
         trn_lbls.append(lbl)
 
     # add the remaining data to the test data sets
@@ -60,34 +61,42 @@ def learn(target_name="George_W_Bush", target_count=530):
     test_lbls = np.array(test_lbls)
 
     # construct the model
-    opt = keras.optimizers.Adam(learning_rate=0.0005)
+    opt = keras.optimizers.Adam(learning_rate=5e-4)
     model = keras.Sequential()
-    model.add(Convolution2D(20, kernel_size=(3, 3), input_shape=input_shape))
+    model.add(Convolution2D(30, kernel_size=(3, 3), input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(keras.layers.BatchNormalization())
-    model.add(Convolution2D(40, kernel_size=(3, 3)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Convolution2D(60, kernel_size=(3, 3)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Convolution2D(80, kernel_size=(3, 3)))
+    model.add(Convolution2D(90, kernel_size=(3, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(120, kernel_size=(3, 3)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(150, kernel_size=(3, 3)))
     model.add(Flatten())
-    model.add(Dense(100))
+    # model.add(Dense(300))
     model.add(Dropout(.74))
     model.add(Dense(1, activation='sigmoid'))  # Output Layer. 1==target, 0==not target
     model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
 
     # train the model
     print("{}'s in the train dataset: {}/{}".format(target_name, np.sum(trn_lbls), len(trn_lbls)))
-    model.fit(trn_data, trn_lbls, epochs=6)
+    model.fit(trn_data, trn_lbls, verbose=1, epochs=4)
 
     return model, test_data, test_lbls
 
 
-if __name__ == "__main__":   
-    cnn, data, labels = learn()
+if __name__ == "__main__":
+    accuracies = []
 
-    # Evaluate model on test data
-    print("\nFINAL EVALUATION RESULT...\n")
-    print("Pictures of the target in the test dataset: {}/{}".format(np.sum(labels), len(labels)))
-    acc = cnn.evaluate(x=data, y=labels)[1]
+    for _ in range(5):
+        cnn, data, labels = learn()
 
+        # Evaluate model on test data
+        print("\nFINAL EVALUATION RESULT...\n")
+        print("Pictures of the target in the test dataset: {}/{}".format(np.sum(labels), len(labels)))
+        acc = cnn.evaluate(x=data, y=labels)[1]
+        accuracies.append(round(acc, 4))
+
+    print(accuracies)
+    print("%.4f" % (sum(accuracies) / len(accuracies)))
